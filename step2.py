22a3a18fcc98f88960f579533007b80ccb7e079e
zaniar ghaderi <53557767+zaniargh42@users.noreply.github.com>
2020-11-12 00:37:51 +0330

    Update step2.py

--- step2.py
+++ step2.py
@@ -1,9 +1,9 @@
 npdata=[]
-
-
 for i in imageaddress: 
     # load the image
     image = Image.open(i)
     # convert image to numpy array
     data = asarray(image)
     npdata.append(data)
+    
+data = np.array(npdata)
